89e23b1faf61113f8f01f552caf094345cdcb741
Md Anwarul Kabir <mdanwarulkabir@gmail.com>
2023-08-14 23:54:15 -0400

    Update Message.java

--- Project-main/BrainGame/src/Message.java
+++ Project-main/BrainGame/src/Message.java
@@ -57,7 +57,7 @@ public class Message {
                 gameIntro=gameIntro+"without answering any questions of any category among Math, IQ, and Riddle.\n";
                 gameIntro=gameIntro+"Or, you might have the chance of answering 5 questions from any of those categories.\n";
                 gameIntro=gameIntro+"Each correct answer will add 10 points to your score, if you can score 50 then you win!\n";
-                gameIntro=gameIntro+"Another exciting part is that your time will be calculated to compare with other players.\n";
+                gameIntro=gameIntro+"Another exciting part is in plan to add, that is your time calculation to compare with other players.\n";
                 gameIntro=gameIntro+"So that you can know that who has completed the game by the lesser amount of time.\n";
                 gameIntro=gameIntro+"We hope that you have a clear idea about this game now, best of luck!\n\n" + reset;
                 
